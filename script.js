d6c433840819859b4b070874fbd064eb96318f52
Angellyn218 <angellyncervantes@yahoo.com>
2023-07-07 15:42:37 -0700

    pressKey function

--- script.js
+++ script.js
@@ -15290,7 +15290,10 @@ const dictionary = [
     "shave"
   ];
 
-startInteracton();
+const WORD_LENGTH = 5;
+const guessGrid = document.querySelector("[data-guess-grid]")
+
+
   
 function startInteracton() {
     document.addEventListener("click", handleMouseClick);
@@ -15336,3 +15339,18 @@ function handleKeyPress(e) {
     }
 }
 
+function pressKey(key) {
+    const activeTiles = getActiveTiles();
+    if (activeTiles.length >= WORD_LENGTH) return;
+    const nextTile = guessGrid.querySelector(":not([data-letter])");
+    nextTile.dataset.letter = key.toLowerCase();
+    nextTile.textContent = key;
+    nextTile.dataset.state = "active";
+
+}
+
+function getActiveTiles() {
+    return guessGrid.querySelectorAll['data-state="active"'];
+}
+
+startInteracton();
